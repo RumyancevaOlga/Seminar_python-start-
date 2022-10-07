a0e1eb6f68b2ede26c19a866e580374cd0acb1d9
Rumyanceva <nj89.89@mail.ru>
2022-10-07 21:23:57 +0300

    Add change in Task1 in HomeWork5

--- HomeWork5/Task1.py
+++ HomeWork5/Task1.py
@@ -27,17 +27,20 @@ user_number = int(input('Введите количество слов: '))
 def get_offer(user_number):
     word = 'абв'
     result = []
-    for i in range(user_number + 1):
+    for i in range(user_number):
         temp = random.sample(word, k = 3)
         result.append(''.join(temp))
-    return result
+    my_list = ' '.join(result)
+    return my_list
 
 def clean_list(arr):
     word = 'абв'
+    arr = arr.split(' ')
     for i in arr:
         if word in arr:
             arr.remove(word)
-    return arr
+    my_list = ' '.join(arr)
+    return my_list
 
 my_list = get_offer(user_number)
 print(my_list)
